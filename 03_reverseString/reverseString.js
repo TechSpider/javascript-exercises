ae251bc9967e96e27af597d55b05d33fd69281da
Rolland Minor <rollandman3@gmail.com>
2024-05-24 13:48:50 +0200

    Complete exercises 01 - 07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let stringReversed = "";
+	
+	for (let i = string.length; i > 0; i--) {
+		stringReversed = stringReversed.concat(string.at(i - 1));
+	}
+	return stringReversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemToRemove) {
+    let newArray = array;
 
+    for (const item of itemToRemove) {
+        for (const element of array) {
+            if (element === item) {
+                newArray = newArray.toSpliced(newArray.indexOf(element), 1);
+            }
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+console.log(removeFromArray([1,2,3,4,5,5,6,6], 6, 5));
